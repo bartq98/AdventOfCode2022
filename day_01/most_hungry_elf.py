558a17813ae10a0bc69b30131f31b6eb0aac2ff6
bartq98 <bartusiekkrosno@poczta.fm>
2022-12-05 01:08:43 +0100

    Add base version for day_01

---
+++ day_01/most_hungry_elf.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+elve_calories_intake = []
+
+with open("./example_data_from_aoc_webpage.txt", 'r') as food_calories_data:
+    totall_intake_of_current_elve = 0
+    for single_food_calories in food_calories_data:
+        if single_food_calories == "\n":
+            elve_calories_intake.append(totall_intake_of_current_elve)
+            totall_intake_of_current_elve = 0
+            continue
+        totall_intake_of_current_elve += int(single_food_calories.strip())
+
+print(sorted(elve_calories_intake, reverse=True)[0])
